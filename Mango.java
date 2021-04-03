df99a7ba3b6957c29af6e7e265e99ea28aa98b43
Jyotiprasad Mohanty <81829420+Jyotiprasad9481@users.noreply.github.com>
2021-04-03 10:50:48 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,25 @@
+class Mango
+{
+String clr , type;
+int no;
+
+public static void main(String args[])
+{
+Mango m1=new Mango();
+m1.clr= "Black";
+m1.type= "round";
+m1.no= 4;
+System.out.println(m1.clr);
+System.out.println(m1.type);
+System.out.println(m1.no);
+System.out.println();
+Mango m2=new Mango();
+m2.clr= "Orange";
+m2.type= "Eclipse";
+m2.no= 8;
+System.out.println(m2.clr);
+System.out.println(m2.type);
+System.out.println(m2.no);
+
+}
+}
